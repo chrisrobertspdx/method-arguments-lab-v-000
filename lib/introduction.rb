ea8897b77f383110c958afa7588e56c5848d81a6
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-20 20:48:54 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,9 @@
 def introduction(name)
-  puts `Hello #{name}!`
+  puts "Hello #{name}!"
 end
 
 def introduction_with_language(name,language)
-  puts `#{name}, how do you say goodbye in #{language}?`
+  puts "#{name}, how do you say goodbye in #{language}?"
 end
+
+introduction("Dan")
